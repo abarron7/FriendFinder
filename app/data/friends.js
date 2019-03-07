963dd966ba1e83b2b1259ee67c9565a649940eda
Alex Barron <abarron4544@yahoo.com>
2019-03-07 01:02:13 -0700

    working

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArr = [
+	{
+	  "name":"Alex",
+	  "photo":"https://media.licdn.com/dms/image/C4E03AQEOB4my2fjLtw/profile-displayphoto-shrink_200_200/0?e=1557360000&v=beta&t=E4MyQ0dfOiLYwB33DEDDgSuSjrsICxGhNVj8JCAD7I4",
+	  "scores[]":[
+	      5,
+	      1,
+	      4,
+	      4,
+	      5,
+	      1,
+	      2,
+	      5,
+	      4,
+	      1
+	    ]
+	}
+];
+
+
+module.exports = friendsArr;
